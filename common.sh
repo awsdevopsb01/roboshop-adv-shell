56a7065dffa2081b6a85b316543a7a6638426c5b
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-01 20:05:48 +0530

    Functions args

--- common.sh
+++ common.sh
@@ -13,27 +13,27 @@ func_setup_nodejs() {
  dnf module enable nodejs:18 -y
 
  func_setup_header "Install NodeJs "
-dnf install nodejs -y
+ dnf install nodejs -y
 
  func_setup_header "Create a Functional User "
-useradd ${func_user}
+ useradd ${func_user}
 
-func_setup_header "Create App Directory "
-rm -rf /app
-mkdir /app
+ func_setup_header "Create App Directory "
+ rm -rf /app
+ mkdir /app
 
-func_setup_header "Download App Content "
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+ func_setup_header "Download App Content "
+ curl -L -o /tmp/"${component}".zip https://roboshop-artifacts.s3.amazonaws.com/"${component}".zip
 
 func_setup_header "Extract App Content "
 cd /app
-unzip /tmp/${component}.zip
+unzip /tmp/"${component}".zip
 
 func_setup_header "Install Dependencies "
 npm install
 
 func_setup_header "Copy Cart Service "
-cp $script_path/cart.service /etc/systemd/system/cart.service
+cp "$script_path"/cart.service /etc/systemd/system/cart.service
 
 func_setup_header "Enable and Start Cart Service "
 systemctl daemon-reload
