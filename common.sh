69479fdf8e13a479deefeb6f24c81adc58cfbfa2
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-02 20:39:15 +0530

    DRY changes Redirectors

--- common.sh
+++ common.sh
@@ -20,32 +20,32 @@ func_status_check() {
 func_load_schema() {
  if [ "$load_schema" == "mongo" ]; then
   func_setup_header "Copy Mongodb repo"
-  cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+  cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
   func_status_check $?
 
   func_setup_header "Install mongodb"
-  dnf install mongodb-org-shell -y
+  dnf install mongodb-org-shell -y &>> /tmp/roboshop.log
   func_status_check $?
 
   func_setup_header "Load Mongodb"
-  mongo --host mongodb-dev.nldevopsb01.online </app/schema/"${component}".js
+  mongo --host mongodb-dev.nldevopsb01.online </app/schema/"${component}".js &>> /tmp/roboshop.log
   func_status_check $?
 
  elif [ "$load_schema" == "mysql" ]; then
   func_setup_header "Install Mysql"
-  dnf install mysql -y
+  dnf install mysql -y &>> /tmp/roboshop.log
   func_status_check $?
 
   func_setup_header "Load Mysql Schema"
-  mysql -h mysql-dev.nldevopsb01.online -uroot -pRoboShop@1 < /app/schema/"${component}".sql
+  mysql -h mysql-dev.nldevopsb01.online -uroot -pRoboShop@1 < /app/schema/"${component}".sql &>> /tmp/roboshop.log
   func_status_check $?
  fi
 }
 
 func_setup_nodejs() {
  func_setup_header "Setup NodeJs Version"
- dnf module disable nodejs -y
- dnf module enable nodejs:18 -y
+ dnf module disable nodejs -y &>> /tmp/roboshop.log
+ dnf module enable nodejs:18 -y &>> /tmp/roboshop.log
  func_status_check $?
 
  func_setup_header "Install NodeJs "
@@ -65,17 +65,17 @@ func_setup_nodejs() {
 func_java_setup() {
 
   func_setup_header "Install Maven"
-  dnf install maven -y
+  dnf install maven -y &>> /tmp/roboshop.log
   func_status_check $?
 
   func_app_setup
 
   func_setup_header "Install Dependencies"
-  mvn clean package
+  mvn clean package &>> /tmp/roboshop.log
   func_status_check $?
 
   func_setup_header "Move Shipping.jar file"
-  mv target/shipping-1.0.jar shipping.jar
+  mv target/shipping-1.0.jar shipping.jar &>> /tmp/roboshop.log
   func_status_check $?
 
   func_load_schema
@@ -93,25 +93,25 @@ func_java_setup() {
   func_status_check $?
 
   func_setup_header "Download Application Content "
-  curl -L -o /tmp/"${component}".zip https://roboshop-artifacts.s3.amazonaws.com/"${component}".zip
+  curl -L -o /tmp/"${component}".zip https://roboshop-artifacts.s3.amazonaws.com/"${component}".zip &>> /tmp/roboshop.log
   func_status_check $?
 
   func_setup_header "Extract Application Content "
   cd /app
-  unzip /tmp/"${component}".zip
+  unzip /tmp/"${component}".zip &>> /tmp/roboshop.log
   func_status_check $?
  }
 
  func_start_systemd() {
     func_setup_header "Copy Application Service "
-    cp "$script_path"/"${component}".service /etc/systemd/system/"${component}".service
+    cp "$script_path"/"${component}".service /etc/systemd/system/"${component}".service &>> /tmp/roboshop.log
     func_status_check $?
 
     func_setup_header "Enable and Start Application Service "
-    systemctl daemon-reload
+    systemctl daemon-reload &>> /tmp/roboshop.log
     func_status_check $?
-    systemctl enable "${component}"
+    systemctl enable "${component}" &>> /tmp/roboshop.log
     func_status_check $?
-    systemctl restart "${component}"
+    systemctl restart "${component}" &>> /tmp/roboshop.log
     func_status_check $?
  }
